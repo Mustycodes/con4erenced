ae6fe6aad51cfe7dd7803f252ee4ef26debcad96
Mustapha Oyenuga <musty1k@yahoo.com>
2024-05-29 00:01:35 +0100

    restyled the navbar

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -11,21 +11,24 @@ const NavLinks = () => {
     `${pathname === path ? activeClass : ""} pb-1`;
 
   return (
-    <ul className='flex space-x-4'>
-      <li>
-        <Link className={getClassStyle("/all-events")} href='/all-events'>
-          Find Events
-        </Link>
-      </li>
-      <li>
-        <Link
-          className={getClassStyle('/create-event')}
-          href='/create-event'
-        >
-          Create Event
-        </Link>
-      </li>
-    </ul>
+    <nav className='hidden md:flex md:items-center md:space-x-14'>
+      <ul className='flex space-x-4'>
+        <li>
+          <Link className={getClassStyle("/all-events")} href='/all-events'>
+            Find Events
+          </Link>
+        </li>
+        <li>
+          <Link className={getClassStyle("/create-event")} href='/create-event'>
+            Create Event
+          </Link>
+        </li>
+      </ul>
+      <div className='flex space-x-4 items-center'>
+        <Link href='/sign-up' className="button bg-white border-2 border-orange-500 py-2 px-4 text-orange-500 rounded ">Create Account</Link>
+        <Link href='/login' className="button bg-orange-500 py-2 px-4 text-white rounded ">Login</Link>
+      </div>
+    </nav>
   );
 };
 
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import { MdMenu } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <nav className='fixed w-full top-0 left-0 bg-white z-30'>
-      <div className='flex justify-between py-4 max-w-[1100px] mx-auto'>
-        <Link href='/' className='font-semibold italic text-lg'>
+    <header className='fixed w-full top-0 left-0 z-20'>
+      <div className='flex justify-between items-center p-4 max-w-screen-xl lg:px-0 mx-auto'>
+        <Link href='/' className='font-semibold italic text-lg flex-1'>
           Con<span className='text-orange-600 font-bold text-2xl'>4</span>
           erenced
         </Link>
+        <button type='button' className="md:hidden">
+          <MdMenu className='text-3xl' />
+        </button>
         <NavLinks />
-        <div>
-          <Link href="/login">Login</Link>
-        </div>
       </div>
-    </nav>
+    </header>
   );
 };
 
