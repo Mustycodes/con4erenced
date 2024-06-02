9cb60ff0c042e3f54b58709624edfcc270903261
Mustapha Oyenuga <musty1k@yahoo.com>
2024-06-02 22:04:12 +0100

    design sign up page

---
+++ app/(routes)/(auth)/_components/SignUpForm.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const SignUpForm = () => {
+  const [loginData, setLoginData] = useState({ username: "", password: "" });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setLoginData((prevLoginData) => ({
+      ...prevLoginData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(loginData);
+  };
+  return (
+    <form onSubmit={handleSubmit} className='flex flex-col space-y-3'>
+      <div className='flex flex-col gap-y-2'>
+        <label htmlFor='username' className='text-lg text-[#F1F2F6]'>
+          Name
+        </label>
+        <input
+          name='username'
+          type='text'
+          className='bg-white bg-opacity-80 border p-2 w-full text-black rounded'
+          onChange={handleChange}
+        />
+      </div>
+      <div className='flex flex-col gap-y-2'>
+        <label htmlFor='username' className='text-lg text-[#F1F2F6]'>
+          Email
+        </label>
+        <input
+          name='username'
+          type='text'
+          className='bg-white bg-opacity-80 border p-2 w-full text-black rounded'
+          onChange={handleChange}
+        />
+      </div>
+      <div className='flex flex-col'>
+        <label htmlFor='password' className='text-lg text-[#F1F2F6]'>
+          Password
+        </label>
+        <input
+          name='password'
+          type='text'
+          className='bg-white bg-opacity-80 border p-2 w-full text-black rounded'
+          onChange={handleChange}
+        />
+      </div>
+      <div className='flex flex-col'>
+        <label htmlFor='password' className='text-lg text-[#F1F2F6]'>
+          Confirm Password
+        </label>
+        <input
+          name='password'
+          type='text'
+          className='bg-white bg-opacity-80 border p-2 w-full text-black rounded'
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <button
+          type='submit'
+          className='w-full bg-orange-500 hover:bg-orange-600 cursor-pointer rounded p-2 text-white font-semibold text-lg'
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SignUpForm;
--- app/(routes)/(auth)/sign-up/page.tsx
+++ app/(routes)/(auth)/sign-up/page.tsx
@@ -1,68 +1,54 @@
-"use client"
+"use client";
+import Wrapper from "@/app/components/ui/Wrapper";
 import Link from "next/link";
 import React, { useState } from "react";
+import SignUpForm from "../_components/SignUpForm";
 
 const SignUpPage = () => {
-    const [signupData, setsignupData] = useState({ username: "", password: "" });
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setsignupData((prevsignupData) => ({
-          ...prevsignupData,
-          [name]: value,
-        }));
-      };
-      const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(signupData);
-      };
+  const [signupData, setsignupData] = useState({ username: "", password: "" });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setsignupData((prevsignupData) => ({
+      ...prevsignupData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(signupData);
+  };
   return (
-    <div className='flex min-h-screen'>
-      <div className='flex-1'>Background HERE</div>
-      <div className='flex-1 flex justify-center items-center'>
-        <div>
-          <h2 className='font-bold text-3xl mb-6'>Create an account</h2>
-          <form onSubmit={handleSubmit} className='flex flex-col space-y-4'>
-            <div className='flex flex-col gap-y-2'>
-              <label htmlFor='username' className='text-lg text-gray-600'>
-                Username
-              </label>
-              <input
-                name='username'
-                type='text'
-                className='border p-2 border-gray-500 w-full rounded'
-                onChange={handleChange}
-              />
-            </div>
-            <div className='flex flex-col'>
-              <label htmlFor='password' className='text-lg text-gray-600'>
-                Password
-              </label>
-              <input
-                name='password'
-                type='text'
-                className='border p-2 border-gray-500 w-full rounded'
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <button
-                type='submit'
-                className='w-full bg-orange-500 hover:bg-orange-600 cursor-pointer p-2 text-white font-semibold text-lg'
-              >
-                Login
-              </button>
-            </div>
-          </form>
-          <div className='mt-2'>
-            <p>
-              Remember your password?{" "}
-              <Link href='/login' className='text-orange-500'>
-                Login
-              </Link>
+    <div className='flex justify-center items-center min-h-screen overflow-auto lg:overflow-hidden bg-[#1C0221] text-slate-200'>
+      <Wrapper className='px-6 py-6 lg:p-0 flex flex-col lg:flex-row'>
+        <div className='flex-1 flex justify-center items-center mb-12'>
+          <div className='text-white text-center lg:text-left w-full md:w-3/4 flex flex-col space-y-4'>
+            <p className='text-4xl font-bold tracking-wide'>
+             Create an account and <br /> start networking 
+            </p>
+            <p className='text-sm lg:text-base text-slate-300 '>
+              Discover and book various events around you all from the comfort
+              of your home. Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Ex, dolorem
             </p>
           </div>
         </div>
-      </div>
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='bg-white bg-opacity-10 p-6 md:p-8 w-full lg:min-w-[400px] max-w-[500px] lg:w-3/5 rounded-lg shadow-lg'>
+            {/* <h2 className='font-bold text-2xl lg:text-3xl mb-6 text-[#F1F2F6]'>
+              Create an account
+            </h2> */}
+            <SignUpForm />
+            <div className='mt-4'>
+              <p className='text-xs lg:text-sm'>
+                Remember your password?{" "}
+                <Link href='/login' className='text-orange-500'>
+                  Login
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
     </div>
   );
 };
